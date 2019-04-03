b49186f7a93ebad457e9726694719ba4fa71b411
geekluxun <geekluxun@163.com>
2019-04-03 17:24:49 +0800

    Initial commit

---
+++ src/test/java/com/geekluxun/myadmin/MyAdminApplicationTests.java
@@ -0,0 +1,16 @@
+package com.geekluxun.myadmin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyAdminApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
